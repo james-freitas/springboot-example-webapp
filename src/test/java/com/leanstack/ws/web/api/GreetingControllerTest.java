d9b4c54923aeba1c6460592499fcc2c056600b62
James Daniel Correia de Freitas <jamesfrj@yahoo.com.br>
2016-12-09 22:30:18 -0200

    Tests

--- src/test/java/com/leanstack/ws/web/api/GreetingControllerTest.java
+++ src/test/java/com/leanstack/ws/web/api/GreetingControllerTest.java
@@ -1,6 +1,7 @@
 package com.leanstack.ws.web.api;
 
 import com.leanstack.ws.AbstractControllerTest;
+import com.leanstack.ws.model.Greeting;
 import com.leanstack.ws.service.GreetingService;
 import org.junit.Assert;
 import org.junit.Before;
@@ -70,13 +71,84 @@ public class GreetingControllerTest extends AbstractControllerTest {
 
         Assert.assertEquals("faiulre - expected HTTP status 404", 404, status);
         Assert.assertTrue("failure - expected HTTP response body to be empty", content.trim().length() == 0);
-
-
     }
 
 
+    @Test
+    public void testCreateGreeting() throws Exception {
 
+        String uri = "/api/greetings";
+        Greeting greeting = new Greeting();
+        greeting.setText("test");
+        String inputJson = super.mapToJson(greeting);
 
+        MvcResult result = mvc
+                .perform(MockMvcRequestBuilders.post(uri)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON).content(inputJson))
+                .andReturn();
+
+        String content = result.getResponse().getContentAsString();
+        int status = result.getResponse().getStatus();
+
+        Assert.assertEquals("failure - expected HTTP status 201", 201, status);
+        Assert.assertTrue("failure - expected HTTP response body to have a value", content.trim().length() > 0);
+
+        Greeting createdGreeting = super.mapFromJson(content, Greeting.class);
+
+        Assert.assertNotNull("failure - expected greeting not null", createdGreeting);
+        Assert.assertNotNull("failure - expected greeting.id not null", createdGreeting.getId());
+        Assert.assertEquals("failure - expected greeting.text match", "test", createdGreeting.getText());
+    }
+
+    @Test
+    public void testUpdateGreeting() throws Exception {
+
+        String uri = "/api/greetings/{id}";
+        Long id = new Long(1);
+        Greeting greeting = greetingService.findOne(id);
+        String updatedText = greeting.getText() + " test";
+        greeting.setText(updatedText);
+        String inputJson = super.mapToJson(greeting);
+
+        MvcResult result = mvc
+                .perform(MockMvcRequestBuilders.put(uri, id)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON).content(inputJson))
+                .andReturn();
+
+        String content = result.getResponse().getContentAsString();
+        int status = result.getResponse().getStatus();
+
+        Assert.assertEquals("failure - expected HTTP status 200", 200, status);
+        Assert.assertTrue("failure - expected HTTP respnse body to have a value", content.trim().length() > 0);
+
+        Greeting updatedGreeting = super.mapFromJson(content, Greeting.class);
+
+        Assert.assertNotNull("failure - expected greeting not null", updatedGreeting);
+        Assert.assertEquals("failure - expected greeting.id unchanged", greeting.getId(),  updatedGreeting.getId());
+        Assert.assertEquals("failure - expected updated greeting text match", updatedText, updatedGreeting.getText());
+    }
+
+    @Test
+    public void testDeleteGreeting() throws Exception {
+        String uri = "/api/greetings/{id}";
+        Long id = new Long(1);
+
+        MvcResult result = mvc.perform(MockMvcRequestBuilders.delete(uri, id)
+                .contentType(MediaType.APPLICATION_JSON)).andReturn();
+
+        String content = result.getResponse().getContentAsString();
+        int status = result.getResponse().getStatus();
+
+        Assert.assertEquals("failure - expected HTTP status 204", 204, status);
+        Assert.assertTrue("failure - expected HTTP response body to be empty", content.trim().length() == 0);
+
+        Greeting deletedGreeting = greetingService.findOne(id);
+
+        Assert.assertNull("failure - expected greeting to be null", deletedGreeting);
+
+    }
 
 
 
